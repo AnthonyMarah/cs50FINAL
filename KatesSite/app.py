3c8a9b0fbab4059176e6df9457515f6e76b2af5a
AnthonyMarah <141853745+AnthonyMarah@users.noreply.github.com>
2023-12-27 19:42:08 -0500

    Add files via upload

---
+++ KatesSite/app.py
@@ -0,0 +1,47 @@
+import os
+import re
+
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+
+# flask --app app.py --debug run - to run flask in debug mode
+
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/services")
+def services():
+    return render_template("services.html")
+
+@app.route("/legal")
+def legal():
+    return render_template("legal.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/payment")
+def payment():
+    return render_template("payment.html")
+
+
+
+
---
+++ KatesSite/readme.md
@@ -0,0 +1,4 @@
+# starpointconsultant.com
+#### Video Demo:  <URL HERE>
+#### Description:
+I built an entirely new and improved website for my friend's business with a focus on user experience. I used flask, css, and html. 
